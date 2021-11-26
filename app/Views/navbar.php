b72d88b0ca26ed94e50e37fc304ce6d18c097490
nadiasalsabila68 <nadiasalsabila2201@gmail.com>
2021-11-27 00:31:39 +0700

    logout, update auth

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -12,6 +12,7 @@
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
             </a>
             <a class="btn btn-sm btn-outline-dark" href="<?= site_url('Auth/login')?>">Sign In</a>
+            <a class="btn btn-sm btn-outline-dark" href="<?=base_url('Auth/logout');?>">Sign Up</a>
           </div>
         </div>
 </header>
