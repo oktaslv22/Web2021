b54ec9db23cbaf4236e344e052695f4111b6ab8a
Okta.slv <okta.slv@yahoo.com>
2021-12-09 11:47:28 +0700

    updated

--- app/Views/barang/create.php
+++ app/Views/barang/create.php
@@ -78,7 +78,7 @@ $submit =[
                 <div class="intro-y box p-5">
                 <?= form_open_multipart('Admin/create') ?>
                             <div>
-                                <label>Colour</label>
+                                <label>Product Name</label>
                                 <?= form_input($nama) ?>
                             </div>
                             <div class="mt-3">
